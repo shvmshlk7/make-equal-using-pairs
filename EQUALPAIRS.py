ad0a5eedb63966b35ceb9257312f7a6ceec6ec9f
SHIVAM SHELKE <103065763+shvmshlk7@users.noreply.github.com>
2023-02-23 23:37:15 +0530

    Create EQUALPAIRS.py

---
+++ EQUALPAIRS.py
@@ -0,0 +1,11 @@
+
+for _ in range(int(input())):
+    m=[]
+    n=int(input())
+    a=list(map(int,input().split(" ")))
+    for i in a:
+        s=a.count(i)
+        m.append(s)
+    print(n-max(m))
+    
+
